d1813138aa810e3bbce266fbd9cd2363bece325b
renatoshk <shkulakurenato@gmail.com>
2023-04-02 21:55:21 +0200

    adding error handling

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/renatoshk/go-learning-module/greetings"
+	"log"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	// Get a greeting message and print it.
-	message := greetings.Hello("Renato")
+	message, err := greetings.Hello("Renato")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }

4da7542bc8338b4628c9edc28ede279267fb5f5f
renatoshk <shkulakurenato@gmail.com>
2023-04-08 18:39:12 +0200

    adding greetings for multiple users

--- hello/hello.go
+++ hello/hello.go
@@ -9,8 +9,10 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
+
+	names := []string{"Naruto", "Sasuke", "Itachi"}
 	// Get a greeting message and print it.
-	message, err := greetings.Hello("Renato")
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
